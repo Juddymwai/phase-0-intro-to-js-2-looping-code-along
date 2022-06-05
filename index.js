b2112e15323e049fbf8b553532651ebcea4f642b
Judy Mwai <juddiemukami@gmail.com>
2022-06-05 18:53:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(array, eventName){
+    let newArray = []
+  
+    for (let card = 0; card < array.length; card++) {
+      let message = `Thank you, ${array[card]}, for the wonderful ${eventName} gift!`
+      newArray.push(message);
+    }
+    console.log(newArray)
+    return newArray
+  
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+  
+  //test while loop
+  
+  function countDown (integer){
+    let i = 10;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  countDown();
